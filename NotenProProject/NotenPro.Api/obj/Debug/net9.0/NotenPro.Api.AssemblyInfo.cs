ef1be91f736defc792d5566f22deec75ee1e4250
Sandla187 <d.thielmann@htlkrems.at>
2026-01-14 15:01:59 +0100

    Local

--- NotenProProject/NotenPro.Api/obj/Debug/net9.0/NotenPro.Api.AssemblyInfo.cs
+++ NotenProProject/NotenPro.Api/obj/Debug/net9.0/NotenPro.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotenPro.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05fb7cae4734d23aa000dc61dfb582b7908b989f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af7fe4a0b4e9010c7b082d5f9568781a30e36570")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotenPro.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotenPro.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
